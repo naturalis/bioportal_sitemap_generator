2c71df86d2257da427a57d175d96789ee2824428
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2017-10-31 13:19:37 +0100

    server settings in index.php

--- index.php
+++ index.php
@@ -2,6 +2,12 @@
 	require_once 'NbaSitemap.php';
 	
 	$nba = new \NbaSitemap();
+	
+	// Server settings
+	$nba->setOutputDir('/opt/git/bioportal_sitemap_generator/sitemaps/');
+	$nba->setLogPath('/var/log/bioportal-sitemap.log');
+	$nba->setBioportalClientDir('/opt/git/bioportal-php-client/');
+	
 	$nba->run();
 
 
